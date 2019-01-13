1f95ac89d339d6934147881550dcc300b45b709b
Elma Lopez <elmalopez@Elmas-MacBook-Pro.local>
2019-01-13 15:47:15 -0800

    request

--- liri-node.js
+++ liri-node.js
@@ -15,3 +15,11 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
 console.log(data); 
 });
 // Note: The limit property is optional and the search will default to 20 if one is not supplied.
+
+// request
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
