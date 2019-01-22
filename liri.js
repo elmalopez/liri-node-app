9f66ecdcec27b432928f28b258aeb1e27eba08e9
Elma Lopez <elmalopez@Elmas-MacBook-Pro.local>
2019-01-21 19:55:44 -0800

    completed

--- liri.js
+++ liri.js
@@ -2,28 +2,82 @@
 require('dotenv').config();
 fs = require('fs');
 keys = require('./keys');
-var songName= process.argv[3];
-var movie = process.arg[3];
+request = require("request");
+
+var request = require("request");
 
 
-Spotify = require('node-spotify-api');
+var userCommand = process.argv[2];
+if (userCommand === "spotify-this-song"){
+  var songName = process.argv[3];
 
-var spotify = new Spotify(keys.spotify);
-// Make it so liri.js can take in one of the following commands:
-// `spotify-this-song`
-// `movie-this`
-// `do-what-it-says`
+  Spotify = require('node-spotify-api');
 
-spotify.search({ type: 'track', query: songName }, function(err, data) {
+  var spotify = new Spotify(keys.spotify);
+
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
+    var songs = data.tracks.items;
     
-  var songs = data.tracks.items; 
-  for(var i = 0; i < songs.length; i++){
-    console.log('artist:' + songs[i].album.artists[0].name);
-    console.log('album:' + songs[i].album.name);
-  }
+      console.log('artist:' + songs[0].album.artists[0].name);
+      console.log('album:' + songs[0].album.name);
+    
 
   });
+}
+else if (userCommand==="do-what-it-says"){
+  var songName = "I Want it That Way";
+
+  Spotify = require('node-spotify-api');
+
+  var spotify = new Spotify(keys.spotify);
+  // Make it so liri.js can take in one of the following commands:
+  // `spotify-this-song`
+  // `movie-this`
+  // `do-what-it-says`
+
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    var songs = data.tracks.items;
+    
+      console.log('artist:' + songs[0].album.artists[0].name);
+      console.log('album:' + songs[0].album.name);
+  
+
+  });
+}
+else {
+  var movieName = process.argv[3];
+ 
+
+  requestUrl = "http://www.omdbapi.com/?apikey=trilogy&t=" + movieName;
+  request(requestUrl, function (err, res,data) {
+    if (err) {
+      return console.log('Error ocurred: ' + err);
+    }
+    var body =JSON.parse(data);
+   console.log(body.Title);
+   console.log(body.Released);
+  // console.log(body.Rotten Tomatoes);
+   console.log(body.imdbRating);
+  //  Country where the movie was produced.
+  console.log(body.Production);
+  // * Language of the movie.
+  console.log(body.Language);
+  // * Plot of the movie.
+  console.log(body.Actors);
+  // * Actors in the movie.
+
+  });
+}
+
+
+
+
+
+
 
